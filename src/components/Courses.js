48d2298fb42d723e3cbfb5f8b1cb4ca7347aa3a8
Andrei <andreykomolov@gmail.com>
2020-08-26 17:07:48 -0400

    Routing Challenge - Add Routs to HTML, CSS, JavaScript courses page

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { Route, BrowserRouter, Link } from 'react-router-dom';
+
+//App components
+import Course from './courses/Course'
+import CSS from './courses/CSS'
+import HTML from './courses/HTML'
+import JavaScript from './courses/JavaScript'
+
 
 const Courses = () => (
-  <div className="main-content courses">
-    <div className="course-header group">
-      <h2>Courses</h2> 
-      <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
-      </ul>
+  <BrowserRouter>
+    <div className="main-content courses">
+      <div className="course-header group">
+        <h2>Courses</h2>
+        <ul className="course-nav">
+          <li><Link to='/courses/html'>HTML</Link></li>
+          <li><Link to='/courses/css'>CSS</Link></li>
+          <li><Link to='/courses/javascript'>JavaScript</Link></li>
+        </ul>
+      </div>
+
+      {/* Write routes here... */}
+      <Route path="/courses/html" component={HTML} />
+      <Route path="/courses/css" component={CSS} />
+      <Route path="/courses/javascript" component={JavaScript} />
     </div>
-    
-    {/* Write routes here... */}
-  </div>
+  </BrowserRouter>
 );
 
 export default Courses;
